7278a1869f82bbbcf6caca2a94ce940afae1a2a0
Hitanshuverma <hatts2001@gmail.com>
2024-06-17 14:49:26 +0530

    prod keys updated

--- src/main/java/com/api/splitTesting/config/RedisConfig.java
+++ src/main/java/com/api/splitTesting/config/RedisConfig.java
@@ -12,10 +12,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    //dev
     @Bean
     public RedisConnectionFactory connectionFactory(){
         return new LettuceConnectionFactory();
     }
+
+    //prod
+//    @Bean
+//    public RedisConnectionFactory connectionFactory() {
+//        //        lettuceConnectionFactory.setDatabase(redisDatabase);
+////        lettuceConnectionFactory.setPassword(redisPassword);
+////        lettuceConnectionFactory.setUsername(redisUsername);
+////        return new LettuceConnectionFactory(redisHost, redisPort);
+//
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+//        lettuceCon
+//    }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
 
